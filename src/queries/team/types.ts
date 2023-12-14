27165da9e003aac98120934ff01d043087c8796f
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-12-14 01:35:02 +0100

    🏷️ Added types for data and employee props

--- src/queries/team/types.ts
+++ src/queries/team/types.ts
@@ -1,1 +1,16 @@
-export type Team = {};
+export type Team = {
+    items: Array<{
+        employees: Array<{
+            value: {
+                name: string,
+                function: string,
+                image: string,
+                office_id: string,
+                birthdate: Date,
+                favorite_pet: string,
+                favorite_food: string,
+            }
+            id: string,
+        }>
+    }>
+};
