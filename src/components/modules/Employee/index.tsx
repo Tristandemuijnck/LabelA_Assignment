c197e050ab4ddfbb89447d114676d036a694eefb
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-12-14 02:09:49 +0100

    ✨ Added employee name & function with styling

--- src/components/modules/Employee/index.tsx
+++ src/components/modules/Employee/index.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import Image from 'next/image';
 
-import { EmployeeContainer } from './styled';
+import { EmployeeContainer, EmployeeInfo, EmployeeName, EmployeeFunction } from './styled';
 
 export const Employee = ({ employee }) => {
   return (
     <EmployeeContainer>
+      <EmployeeInfo>
+        <EmployeeName>{employee.value.name}</EmployeeName>
+        <EmployeeFunction>{employee.value.function}</EmployeeFunction>
+      </EmployeeInfo>
       <Image
         src={`https://cms.labela.nl${employee.value.image.small}`}
         width={488}
--- src/components/modules/Employee/styled.ts
+++ src/components/modules/Employee/styled.ts
@@ -5,4 +5,47 @@ export const EmployeeContainer = styled.div`
   overflow: hidden;
   display: flex;
   align-items: center;
+  position: relative;
+  filter: grayscale(100%);
+  transition: filter 0.3s ease-in-out;
+
+  &:hover {
+    filter: grayscale(0%);
+  }
+`;
+
+export const EmployeeInfo = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 1rem 0.6rem;
+  row-gap: 0.5rem;
+  transform: translateY(35px);
+  transition: transform 0.3s ease-in-out;
+
+  ${EmployeeContainer}:hover & {
+    transform: translateY(0);
+  }
+`
+
+export const EmployeeName = styled.h2`
+  font-size: 1.1rem;
+  color: #80BB00;
+  margin: 0;
+  padding: 0;
+  text-align: right;
+  padding-right: 20px;
+`;
+
+export const EmployeeFunction = styled.p`
+  font-size: 1rem;
+  color: #fff;
+  margin: 0;
 `;
