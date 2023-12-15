58380b6ef50d9af409c41967a122b372587ca928
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-12-15 01:39:31 +0100

    ✨ Added employee filter

--- src/components/modules/EmployeeFilter/index.tsx
+++ src/components/modules/EmployeeFilter/index.tsx
@@ -3,13 +3,27 @@ import * as React from 'react';
 import { Button } from 'common';
 
 import { FilterContainer } from './styled';
+import { log } from 'console';
 
-export const EmployeeFilter = ({ employeeFunctions }) => {
+export const EmployeeFilter = ({ employeeFunctions, onFilter }) => {
+
+  // Filter all duplicates out of the employeeFunctions array
+  const uniqueFunctions = employeeFunctions.filter((value, index, self) => self.indexOf(value) === index);
+
+  // Count how many times each function appears in the array employeeFunctions
+  const countFunctions = uniqueFunctions.map((employeeFunction) => {
+    return {
+      function: employeeFunction,
+      count: employeeFunctions.filter((x) => x === employeeFunction).length,
+    }
+  });
+
   return (
     <FilterContainer>
-      {employeeFunctions.map((employeeFunction) => (
-        <Button key={employeeFunction}>{employeeFunction}</Button>
+      {countFunctions.map((countFunction) => (
+        countFunction.count >= 3 && <Button key={countFunction.function} onClick={() => onFilter(countFunction.function)}>{countFunction.function}</Button>
       ))}
+      <Button onClick={() => onFilter("Other")}>Other</Button>
     </FilterContainer>
   );
 };
--- src/components/modules/Employees/index.tsx
+++ src/components/modules/Employees/index.tsx
@@ -3,13 +3,38 @@ import * as React from 'react';
 import { useGetTeam } from 'queries/team';
 import { Grid, Paragraph } from 'common';
 import { Employee, EmployeeFilter } from 'modules';
+import { log } from 'console';
 
 export const Employees = () => {
   const { isLoading, data } = useGetTeam();
 
   const employees = data?.items[0]?.employees || [];
 
-  const employeeFunctions: string[] = [];
+  const employeeFunctions: string[] = employees.map((employee) => employee.value.function)
+
+  const [filter, setFilter] = React.useState(employees)
+
+  const filterEmployees = (filterString: string) => {
+
+    // If filter string is "Other", filter out all employees with a function that appears 3 or more times
+    if (filterString === "Other") {
+      const uniqueFunctions = employeeFunctions.filter((value, index, self) => self.indexOf(value) === index);
+      const countFunctions = uniqueFunctions.map((employeeFunction) => {
+        return {
+          function: employeeFunction,
+          count: employeeFunctions.filter((x) => x === employeeFunction).length,
+        }
+      });
+      const otherFunctions = countFunctions.filter((countFunction) => countFunction.count <= 2);
+      const filteredEmployees = employees.filter((employee) => otherFunctions.some((otherFunction) => otherFunction.function === employee.value.function))
+      setFilter(filteredEmployees)
+      return
+    } else {
+      const filteredEmployees = employees.filter((employee) => employee.value.function === filterString)
+      setFilter(filteredEmployees)
+      return
+    }
+  }
 
   return (
     <>
@@ -17,9 +42,12 @@ export const Employees = () => {
         <Paragraph>Loading</Paragraph>
       ) : (
         <>
-          <EmployeeFilter employeeFunctions={employeeFunctions} />
+          <EmployeeFilter employeeFunctions={employeeFunctions} onFilter={filterEmployees}/>
           <Grid>
-            {employees.map((employee) => (
+            {filter.length > 0 ? filter.map((employee) => (
+              <Employee employee={employee} key={employee.id} />
+            )) :
+            employees.map((employee) => (
               <Employee employee={employee} key={employee.id} />
             ))}
           </Grid>
